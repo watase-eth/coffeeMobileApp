11c3dc08400728562e88a2bf0612c3cbc731e150
Sean Watase <seanwatase@Seans-MacBook-Pro.local>
2023-04-05 14:16:42 -1000

    Initial commit from npx thirdweb create

---
+++ App.tsx
@@ -0,0 +1,63 @@
+import {
+  ConnectWallet,
+  metamaskWallet,
+  rainbowWallet,
+  ThirdwebProvider,
+} from '@thirdweb-dev/react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <ThirdwebProvider
+      activeChain="mumbai"
+      supportedWallets={[metamaskWallet(), rainbowWallet()]}>
+      <AppInner />
+    </ThirdwebProvider>
+  );
+};
+
+const AppInner = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const textStyles = {
+    color: isDarkMode ? Colors.white : Colors.black,
+    ...styles.heading,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View style={styles.view}>
+        <Text style={textStyles}>React Native thirdweb starter</Text>
+        <ConnectWallet />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
+
+export default App;
